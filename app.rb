f037018badd0b4e648a071a306308e1968405e55
Gunel Mehdiyeva-Chau <gunel.mehdiyev@gmail.com>
2021-05-12 19:45:35 +0100

    post/redirect/get pattern

--- app.rb
+++ app.rb
@@ -5,13 +5,25 @@ require 'sinatra/reloader'
 require 'rack'
 
 class Battle < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end 
+
+  enable :sessions
+
   get '/' do
     erb :index
   end
 
   post '/names' do
-    @player_1_name = params[:player_1_name]
-    @player_2_name = params[:player_2_name]
-    erb :play
+    session[:player_1] = params[:player_1_name]
+    session[:player_2] = params[:player_2_name]
+    redirect '/play'
   end
+
+ get '/play' do
+    @player_1_name = session[:player_1]
+    @player_2_name = session[:player_2]
+    erb :play
+  end 
 end
--- spec/features/entering_players_spec.rb
+++ spec/features/entering_players_spec.rb
@@ -9,6 +9,9 @@ feature 'entering players' do
     fill_in('player_1_name', with: 'John')
     fill_in('player_2_name', with: 'Becca')
     click_button('Submit')
+
+    save_and_open_page
+    
     expect(page).to have_content 'John vs Becca' 
   end
 end
