2f19c433af13faf94b2a76064d4d3496b685d8cd
Gunel Mehdiyeva-Chau <gunel.mehdiyev@gmail.com>
2021-05-13 15:03:10 +0100

    refactor the code

--- app.rb
+++ app.rb
@@ -15,14 +15,14 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    $player_1_name = Player.new(params[:player_1_name])
-     $player_2_name = Player.new(params[:player_2_name])
+    $player_1 = Player.new(params[:player_1_name])
+     $player_2 = Player.new(params[:player_2_name])
     redirect '/play'
   end
 
  get '/play' do
-    @player_1_name = $player_1_name.name
-    @player_2_name = $player_2_name.name
+    @player_1_name = $player_1.name
+    @player_2_name = $player_2.name
     erb :play
   end
 
